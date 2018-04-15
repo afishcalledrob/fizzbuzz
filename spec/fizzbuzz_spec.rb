4bc973bb1fad1c47fb64f62009ea9438fe4d6183
Robert <fishwickrob@live.com>
2018-04-15 12:19:59 +0000

    Passed third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 9' do
     expect(fizzbuzz(9)).to eq 'fizz'
   end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
